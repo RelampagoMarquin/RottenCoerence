9694525ec54ff1608b4cd20d2cec62f83953ec7d
Marcos Moreira <marcos_more@outlook.com>
2023-12-22 16:04:03 -0300

    Base

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # RottenCoerence
 Atividade final da disciplina de analise de dados
+
+
+# Pacotes necessarios para executar:
+- TextBlob
+- requests
+- BeautifulSoup4
+- Dash
+- pandas
+- datetime
+- numpy
+- plotly.express
+- plotly.graph_objs
+- from collections import deque
---
+++ main.py
@@ -0,0 +1,11 @@
+from textblob import TextBlob
+import requests
+from bs4 import BeautifulSoup
+from dash import Dash, html, dcc, dash_table, callback, Output, Input
+import pandas as pd
+import datetime
+import numpy as np
+import plotly.express as px
+import plotly.graph_objs as go
+from collections import deque
+
